067383b7b7d4095425608cfa7f299e4ddfbe0c3f
kunal mittal <kanumittal99@gmail.com>
2018-07-23 13:00:23 +0530

    ready for push

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+ 
+// default options
+app.use(fileUpload());
+ 
+app.post('/upload', function(req, res) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  //console.log(req.files);
+  for(let fileo of req.files.foo){
+    let name=fileo.name; 
+    fileo.mv('./server/'+name,function(err){
+        if (err)
+            return res.status(500).send(err);
+      });
+  }
+  res.send('File uploaded!');
+});
+app.listen(3000,()=>{
+    console.log('server started on port 3000');
+});
